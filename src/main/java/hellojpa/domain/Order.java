7aaaa8f0b1ff74ace3a0cb540079f47187d9344e
조영혜 <0808yeonghye@gmail.com>
2022-06-01 00:55:39 +0900

    Order, Member, Item, OrderItem 파일 생성, 기본 매핑 완성

---
+++ src/main/java/hellojpa/domain/Order.java
@@ -0,0 +1,22 @@
+package hellojpa.domain;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "orders")
+public class Order {
+
+    @Id @GeneratedValue
+    @Column(name = "order_id")
+    private Long id;
+
+    @Column(name = "member_id")
+    private Long memberId;
+
+    private LocalDateTime orderDate;
+
+    @Enumerated(EnumType.STRING) // 꼭 String으로!!
+    private OrderStatus Status;
+
+}
